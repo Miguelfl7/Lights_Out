b09e7372aedb1d0ce459a5927d8d0461b1bc442b
Miguelfl7 <Mirferla@gmail.com>
2021-05-13 03:31:54 +0200

    Mejor el movement

---
+++ Assets/Scripts/CharacterControllerHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CharacterControllerHelper : MonoBehaviour
+{
+    private XRRig XRRig;
+    private CharacterController CharacterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacterController();
+    }
+
+        protected virtual void UpdateCharacterController()
+        {
+            if (xRRig == null || m_CharacterController == null)
+                return;
+
+            var height = Mathf.Clamp(xRRig.cameraInRigSpaceHeight, m_MinHeight, m_MaxHeight);
+
+            Vector3 center = xRRig.cameraInRigSpacePos;
+            center.y = height / 2f + m_CharacterController.skinWidth;
+
+            m_CharacterController.height = height;
+            m_CharacterController.center = center;
+        }
+}
